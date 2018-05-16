51bfe0f91be1571046c3d4f76bb784fa7fd46b54
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-16 16:29:02 +0200

    review and restaurant controllers index, new and create methods

--- Gemfile
+++ Gemfile
@@ -37,6 +37,11 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "restaurants", to: "restaurants#index"
+
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+
+  # get "restaurants/:id", to: "restaurants#show"
+  resources :restaurants, only: [ :new, :create, :show, :index] do
+    resources :reviews, only: [ :new, :create, :index ]
+  end
 end
