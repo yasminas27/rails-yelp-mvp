0a8d0e8abd4694fb30443681ae9c1efdf4f847ad
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-16 13:07:42 +0200

    added 5 new restaurants with 3 reviews each

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Creating 5 fake restaurants..."
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Name.name,
+    address: Faker::RickAndMorty.location,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+    restaurant.save!
+    3.times do
+      review = Review.new(
+        content: Faker::Friends.quote,
+        rating: (0..5).to_a.sample,
+        restaurant_id: restaurant.id
+        )
+      review.save!
+    end
+end
+puts "Done!"
+
